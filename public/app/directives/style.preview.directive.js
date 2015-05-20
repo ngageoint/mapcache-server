2bfc1b6276343a0b343fed7d89d7746e4adeb910
Dan Barela <daniel.barela@gmail.com>
2015-05-20 10:27:58 -0600

    show the preview when the style changes

--- public/app/directives/style.preview.directive.js
+++ public/app/directives/style.preview.directive.js
@@ -27,31 +27,35 @@ function StylePreviewController($scope, $element) {
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
     } : null;
-}
-
-  var canvas = $element[0];
-
-  if (canvas.getContext){
-    var ctx = canvas.getContext('2d');
-    var rgbFill = hexToRgb($scope.stylePreview.fill);
-    ctx.fillStyle = "rgba("+rgbFill.r+","+rgbFill.g+","+rgbFill.b+","+$scope.stylePreview['fill-opacity']+")";
-    ctx.lineWidth = $scope.stylePreview['stroke-width'];
-    var rgbStroke = hexToRgb($scope.stylePreview['stroke']);
-    ctx.strokeStyle = "rgba("+rgbStroke.r+","+rgbStroke.g+","+rgbStroke.b+","+$scope.stylePreview['stroke-opacity']+")";
-
-    ctx.beginPath();
-    ctx.moveTo(10,0);
-    ctx.lineTo(30,10);
-    ctx.lineTo(32,20);
-    ctx.lineTo(15,30);
-    ctx.lineTo(18,18);
-    ctx.lineTo(0,12);
-    ctx.lineTo(10,0);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-
-  } else {
-    console.log('no canvas support');
   }
+
+  $scope.$watch('stylePreview', function() {
+    if (!$scope.stylePreview) return;
+    var canvas = $element[0];
+
+    if (canvas.getContext){
+      var ctx = canvas.getContext('2d');
+      ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+      var rgbFill = hexToRgb($scope.stylePreview.fill);
+      ctx.fillStyle = "rgba("+rgbFill.r+","+rgbFill.g+","+rgbFill.b+","+$scope.stylePreview['fill-opacity']+")";
+      ctx.lineWidth = $scope.stylePreview['stroke-width'];
+      var rgbStroke = hexToRgb($scope.stylePreview['stroke']);
+      ctx.strokeStyle = "rgba("+rgbStroke.r+","+rgbStroke.g+","+rgbStroke.b+","+$scope.stylePreview['stroke-opacity']+")";
+
+      ctx.beginPath();
+      ctx.moveTo(10,0);
+      ctx.lineTo(30,10);
+      ctx.lineTo(32,20);
+      ctx.lineTo(15,30);
+      ctx.lineTo(18,18);
+      ctx.lineTo(0,12);
+      ctx.lineTo(10,0);
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+
+    } else {
+      console.log('no canvas support');
+    }
+  }, true);
 }
