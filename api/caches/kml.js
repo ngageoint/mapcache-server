9b548c94eb5119cb4aa753fa887a883aa026c469
Dan Barela <daniel.barela@gmail.com>
2015-05-26 15:48:30 -0600

    cleaning up bad variable names

--- api/caches/kml.js
+++ api/caches/kml.js
@@ -7,19 +7,19 @@ var CacheModel = require('../../models/cache.js')
   , fs = require('fs-extra');
 
 exports.getCacheData = function(cache, minZoom, maxZoom, callback) {
-  var shapefileZip = path.join(config.server.cacheDirectory.path, cache._id.toString(), cache._id + ".kml");
+  var file = path.join(config.server.cacheDirectory.path, cache._id.toString(), cache._id + ".kml");
 
-  if (!fs.existsSync(shapefileZip)) {
+  if (!fs.existsSync(file)) {
     var child = require('child_process').fork('api/caches/creator.js');
     child.send({operation:'generateCache', cache: cache, format: 'kml', minZoom: minZoom, maxZoom: maxZoom});
     callback(null, {creating: true});
   } else {
-    var stream = fs.createReadStream(shapefileZip);
+    var stream = fs.createReadStream(file);
     callback(null, {stream: stream, extension: '.kml'});
   }
 }
 
 exports.generateCache = function(cache, minZoom, maxZoom, callback) {
-  var shapefileZip = path.join(config.server.cacheDirectory.path, cache._id.toString(), cache._id + ".kml");
-  ogrType.generateCache(cache, shepfileZip, 'KML', callback);
+  var file = path.join(config.server.cacheDirectory.path, cache._id.toString(), cache._id + ".kml");
+  ogrType.generateCache(cache, file, 'KML', callback);
 }
--- api/caches/mbtiles.js
+++ api/caches/mbtiles.js
@@ -5,13 +5,13 @@ var CacheModel = require('../../models/cache.js')
   , fs = require('fs-extra');
 
  exports.getCacheData = function(cache, minZoom, maxZoom, callback) {
-   var geoPackageFile = path.join(config.server.cacheDirectory.path, cache._id.toString(), cache._id + ".mbitles");
-   if (!fs.existsSync(geoPackageFile)) {
+   var mbtilesFile = path.join(config.server.cacheDirectory.path, cache._id.toString(), cache._id + ".mbitles");
+   if (!fs.existsSync(mbtilesFile)) {
      var child = require('child_process').fork('api/caches/creator.js');
      child.send({operation:'generateCache', cache: cache, format: 'mbtiles', minZoom: minZoom, maxZoom: maxZoom});
      callback(null, {creating: true});
    } else {
-     var stream = fs.createReadStream(geoPackageFile);
+     var stream = fs.createReadStream(mbtilesFile);
      callback(null, {stream: stream, extension: '.mbtiles'});
    }
  }
