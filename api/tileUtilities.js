4ef32dd32d963612cbdf2963a3f0e080cb0f8f25
Dan Barela <daniel.barela@gmail.com>
2015-06-10 14:45:29 -0600

    narrowing down leak

--- api/tileUtilities.js
+++ api/tileUtilities.js
@@ -192,7 +192,6 @@ exports.writeVectorTile = function(tile, source, z, x, y, callback) {
     fs.mkdirsSync(dir, function(err){
        if (err) console.log(err);
      });
-		JSON.stringify(tile);
 		callback(null);
 		// fs.writeFile(file, JSON.stringify(tile), function (err) {
 		//   if (err) return console.log(err);
