bf3687f4f8f00a92c8464ab0aed1efdfd6994116
Dan Barela <daniel.barela@gmail.com>
2015-06-18 08:06:31 -0600

    force required and non virtual cache types to be created

--- api/cache.js
+++ api/cache.js
@@ -47,52 +47,67 @@ Cache.prototype.create = function(cache, formats, callback) {
   callback = callback || function(){}
 
   var newFormats = [];
-  if (typeof formats === "string") {
-    console.log('formats is a string', formats);
-    formats = [formats];
-  }
-  console.log('formats', formats);
-  for (var i = 0; i < formats.length; i++) {
-    if (!cache.formats || !cache.formats[formats[i]]) {
-      newFormats.push(formats[i]);
+  if (formats) {
+    if (typeof formats === "string") {
+      console.log('formats is a string', formats);
+      formats = [formats];
+    } else if (Array.isArray(formats)) {
+      for (var i = 0; i < formats.length; i++) {
+        if (!cache.formats || !cache.formats[formats[i]]) {
+          newFormats.push(formats[i]);
+        }
+      }
     }
   }
 
-  // if the request did not specify the required caches
-  // add them here
-
-
   if (cache.id) {
+    // if the request did not specify the required caches
+    // add them here
+    var cacheTypes = config.sourceCacheTypes[cache.source.format];
+    for (var i = 0; i < cacheTypes.length; i++) {
+      var item = cacheTypes[i];
+      if (!newFormats.indexOf(item.type)) {
+        newFormats.push(item.type);
+      }
+    }
     for (var i = 0; i < newFormats.length; i++) {
       console.log("creating format " + newFormats[i] + " for cache " + cache.name);
       cacheProcessor.createCacheFormat(cache, newFormats[i], function(err, cache) {
-        // console.log('format ' + newFormats[i] + ' submitted for cache ' + cache.name);
       });
     }
     return callback(null, cache);
+  } else {
+    cache.tileSizeLimit = cache.tileSizeLimit || config.server.maximumCacheSize * 1024 * 1024;
+
+    cache.status = {
+      complete: false,
+      totalTiles: 0,
+      generatedTiles: 0,
+      totalFeatures: 0,
+      generatedFeatures: 0,
+      zoomLevelStatus: {}
+    };
+
+    CacheModel.createCache(cache, function(err, newCache) {
+      if (err) return callback(err);
+      callback(err, newCache);
+      // if the request did not specify the required caches
+      // add them here
+      var cacheTypes = config.sourceCacheTypes[newCache.source.format];
+      for (var i = 0; i < cacheTypes.length; i++) {
+        var item = cacheTypes[i];
+        if (item.required && !item.virtual && newFormats.indexOf(item.type) == -1) {
+          newFormats.push(item.type);
+        }
+      }
+      for (var i = 0; i < newFormats.length; i++) {
+        console.log("creating format " + newFormats[i] + " for cache " + newCache.name);
+        cacheProcessor.createCacheFormat(newCache, newFormats[i], function(err, cache) {
+          console.log('format ' + newFormats[i] + ' submitted for cache ' + newCache.name);
+        });
+      }
+    });
   }
-
-  cache.tileSizeLimit = cache.tileSizeLimit || config.server.maximumCacheSize * 1024 * 1024;
-
-  cache.status = {
-    complete: false,
-    totalTiles: 0,
-    generatedTiles: 0,
-    totalFeatures: 0,
-    generatedFeatures: 0,
-    zoomLevelStatus: {}
-  };
-
-  CacheModel.createCache(cache, function(err, newCache) {
-    if (err) return callback(err);
-    callback(err, newCache);
-    for (var i = 0; i < newFormats.length; i++) {
-      console.log("creating format " + newFormats[i] + " for cache " + newCache.name);
-      cacheProcessor.createCacheFormat(newCache, newFormats[i], function(err, cache) {
-        console.log('format ' + newFormats[i] + ' submitted for cache ' + newCache.name);
-      });
-    }
-  });
 }
 
 Cache.prototype.restart = function(cache, format, callback) {
