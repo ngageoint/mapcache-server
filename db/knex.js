d589053cac39b17d63d8e56139f4e4d2b87e5d2d
Dan Barela <daniel.barela@gmail.com>
2015-08-18 15:18:23 -0600

    adding indexes to the schema creation code

--- db/knex.js
+++ db/knex.js
@@ -16,11 +16,11 @@ knex.schema.hasTable('features').then(function(exists) {
   if (!exists) {
     return knex.schema.createTable('features', function(featureTable) {
       featureTable.increments('id').primary();
-      featureTable.string('source_id');
-      featureTable.string('cache_id');
+      featureTable.string('source_id').index();
+      featureTable.string('cache_id').index();
       featureTable.json('properties');
-      featureTable.specificType('box', 'geometry');
-      featureTable.specificType('geometry', 'geometry');
+      featureTable.specificType('box', 'geometry').index('features_box_idx', 'GIST');
+      featureTable.specificType('geometry', 'geometry').index('features_geometry_idx', 'GIST');
     });
   }
 });
