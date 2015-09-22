adbd9b1afd01884ac9169f323ad6130b43c75224
Dan Barela <daniel.barela@gmail.com>
2015-09-22 08:30:42 -0600

    show the maps that are complete properly

--- public/app/mapcache/maps.controller.js
+++ public/app/mapcache/maps.controller.js
@@ -16,19 +16,20 @@ function MapsController($scope, $rootScope, LocalStorageService, CacheService, M
 
   $scope.mapOptions = {
     baseLayerUrl: 'http://mapbox.geointapps.org:2999/v4/mapbox.light/{z}/{x}/{y}.png',
-    opacity: .5,
-    // mapOptions: {
-    //   hideZoomIndicator: true,
-    //   dragging: false,
-    //   touchZoom: false,
-    //   scrollWheelZoom: false,
-    //   doubleClickZoom: false,
-    //   zoomControl: false
-    // }
+    opacity: .5
   };
 
   MapService.getAllMaps(true).success(function(maps) {
     $scope.maps = maps;
+
+    _.each($scope.maps, function(map) {
+      if (_.some(map.dataSources, function(value) { return !value.status.complete; })) {
+        map.mapComplete = false;
+      } else {
+        map.mapComplete = true;
+      }
+    });
+
   });
 
 }
