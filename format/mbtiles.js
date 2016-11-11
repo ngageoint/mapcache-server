c42db457c03b45e80b31ae173377974c1c8949a5
Dan Barela <daniel.barela@gmail.com>
2016-11-11 13:14:18 -0700

    use the geopackage to mbtiles converter

--- format/mbtiles.js
+++ format/mbtiles.js
@@ -5,15 +5,80 @@ var log = require('mapcache-log')
   , path = require('path')
   , request = require('request')
   , async = require('async')
-  , cp = require('child_process');
+  , cp = require('child_process')
+  , GeoPackage = require('geopackage');
 
 var MBTiles = function(config) {
   this.config = config || {};
   this.source = config.source;
   this.cache = config.cache;
+  if (this.config.cache && !this.config.outputDirectory) {
+    throw new Error('An output directory must be specified in config.outputDirectory');
+  }
 };
 
-MBTiles.prototype.initialize = function() {
+MBTiles.prototype.generateCache = function(doneCallback, progressCallback) {
+  log.info('Generating cache with id %s', this.cache.cache.id);
+
+  var cacheObj = this.cache;
+  cacheObj.cache.formats = cacheObj.cache.formats || {};
+
+  if (cacheObj.cache.formats.geopackage) {
+    var gpdir = path.join(this.config.outputDirectory, cacheObj.cache.id, 'gpkg');
+    var gpfilename = cacheObj.cache.id + '.gpkg';
+    var gpfilepath = path.join(gpdir, gpfilename);
+
+    var dir = path.join(this.config.outputDirectory, cacheObj.cache.id, 'mbtiles');
+    var filename = cacheObj.cache.id;
+    var mbtilesFilePath = path.join(dir, filename + '.mbtiles');
+    var zipFilePath = path.join(dir, filename + '.zip');
+
+    if (fs.existsSync(mbtilesFilePath)) {
+      log.info('Cache %s already exists, returning', cacheObj.cache.id);
+      var stats = fs.statSync(mbtilesFilePath);
+      cacheObj.cache.formats.mbtiles = {
+        complete: true,
+        percentComplete: 100,
+        size: stats.size
+      };
+      return doneCallback(null, cacheObj);
+    } else if(fs.existsSync(zipFilePath)) {
+      log.info('Cache %s already exists, returning', cacheObj.cache.id);
+      var stats = fs.statSync(zipFilePath);
+      cacheObj.cache.formats.mbtiles = {
+        complete: true,
+        percentComplete: 100,
+        size: stats.size
+      };
+      return doneCallback(null, cacheObj);
+    } else {
+      cacheObj.cache.formats.mbtiles = {
+        complete: false,
+        percentComplete: 0
+      };
+      fs.emptyDirSync(dir);
+      GeoPackage.openGeoPackage(gpfilepath, function(err, gp) {
+        GeoPackage.MBTilesToGeoPackage.extract(gp, undefined, function(err, result, extras) {
+          if (extras && extras.extension) {
+            filename = filename + '.' + extras.extension;
+          } else {
+            filename = filename + '.mbtiles';
+          }
+          var filePath = path.join(dir, filename);
+          fs.writeFile(filePath, result, function(err) {
+            var stats = fs.statSync(filePath);
+            cacheObj.cache.formats.mbtiles.complete = true;
+            cacheObj.cache.formats.mbtiles.percentComplete = 100;
+            cacheObj.cache.formats.mbtiles.size = stats.size;
+            console.log('created the file, call the callback', doneCallback);
+            doneCallback(err, cacheObj);
+          });
+        });
+      });
+    }
+  } else {
+    doneCallback(null, null);
+  }
 };
 
 MBTiles.prototype.processSource = function(doneCallback, progressCallback) {
@@ -123,136 +188,35 @@ MBTiles.prototype._getTileFromSource = function(source, z, x, y, format, callbac
   return callback();
 };
 
-MBTiles.prototype.generateCache = function(callback, progressCallback) {
-  log.info('Generating cache with id %s', this.cache.cache.id);
-  var self = this;
-  callback = callback || function() {};
-  progressCallback = progressCallback || function(cache, callback) {
-    callback(null, cache);
-  };
-  var cache = this.cache.cache;
-  var cacheId = this.cache.cache.id;
-  cache.formats = cache.formats || {};
-  cache.formats.mbtiles = cache.formats.mbtiles || {
-    generatedTiles: 0,
-    complete: false,
-    size: 0,
-    percentComplete: 0
-  };
-
-  var tiles = xyzTileUtils.tilesInFeatureCollection(cache.geometry, cache.minZoom, cache.maxZoom);
-  var totalTiles = 0;
-  cache.formats.mbtiles.zoomLevelStatus = [];
-  for (var i = cache.minZoom; i <= cache.maxZoom; i++) {
-    var zoomTiles = Object.keys(tiles[i]).length;
-    totalTiles += zoomTiles;
-    cache.formats.mbtiles.zoomLevelStatus[i] = {
-      generatedTiles: 0,
-      totalTiles: zoomTiles,
-      complete: false,
-      percentComplete: 0,
-      size: 0
-    };
-  }
-
-  cache.formats.mbtiles.totalTiles = totalTiles;
-  progressCallback(cache, function(err, updatedCache) {
-    cache = updatedCache;
-    xyzTileUtils.iterateTiles(tiles, cache.minZoom, cache.maxZoom, cache, function processTile(tile, tileDone) {
-      // put the tiles into the xyztiles directory because we will then use mbutil to create an mbtiles file from that directory
-      var dir = path.join(self.config.outputDirectory, cacheId.toString(), 'xyztiles', tile.z.toString(), tile.x.toString());
-      var filename = tile.y + '.png';
-
-      log.debug('tile %d %d %d will be written to %s', tile.z, tile.x, tile.y, path.join(dir, filename));
-
-      if (fs.existsSync(path.join(dir, filename)) && (!cache.cacheCreationParams || (cache.cacheCreationParams && !cache.cacheCreationParams.noCache))) {
-        log.debug('file already exists, skipping: %s', path.join(dir, filename));
-        cache.formats.mbtiles.zoomLevelStatus[tile.z].generatedTiles++;
-        cache.formats.mbtiles.zoomLevelStatus[tile.z].percentComplete = 100 * cache.formats.mbtiles.zoomLevelStatus[tile.z].generatedTiles / cache.formats.mbtiles.zoomLevelStatus[tile.z].totalTiles;
-        cache.formats.mbtiles.generatedTiles++;
-        cache.formats.mbtiles.percentComplete = 100 * cache.formats.mbtiles.generatedTiles/cache.formats.mbtiles.totalTiles;
-        progressCallback(cache, function(err, updatedCache) {
-          cache = updatedCache;
-          return tileDone(null, tile);
-        });
-      } else {
-        log.info('the file %s does not exist for the mbtiles cache %s, creating', path.join(dir, filename), cacheId);
-        self.cache.getTile('png', tile.z, tile.x, tile.y, cache.cacheCreationParams, function(err, stream) {
-          if (err) {
-            log.error(err);
-            return tileDone(null, null);
-          } else if (!stream) {
-            log.error('There was no tile for ' + path.join(dir, filename));
-            return tileDone(null, null);
-          }
-          var ws = fs.createOutputStream(path.join(dir, filename));
-          stream.pipe(ws);
-          ws.on('close', function(){
-            log.info('the file %s was written for the mbtiles cache %s', path.join(dir, filename), cacheId);
-            cache.formats.mbtiles.zoomLevelStatus[tile.z].generatedTiles++;
-            cache.formats.mbtiles.zoomLevelStatus[tile.z].percentComplete = 100 *  cache.formats.mbtiles.zoomLevelStatus[tile.z].generatedTiles / cache.formats.mbtiles.zoomLevelStatus[tile.z].totalTiles;
-            cache.formats.mbtiles.generatedTiles++;
-            cache.formats.mbtiles.percentComplete = 100 * cache.formats.mbtiles.generatedTiles/cache.formats.mbtiles.totalTiles;
-            progressCallback(cache, function(err, updatedCache) {
-              cache = updatedCache;
-              return tileDone(null, tile);
-            });
-          });
-        });
-      }
-    }, function zoomCallback(zoom, callback) {
-      log.info('zoom level %d is done for %s', zoom, cacheId);
-      cache.formats.mbtiles.zoomLevelStatus[zoom].complete = true;
-      progressCallback(cache, function(err, updatedCache) {
-        cache = updatedCache;
-        callback();
-      });
-    }, function done(err, data) {
-      log.info('all tiles are done for %s', cacheId);
-      self._createMetadataFile();
-      self.createMBTilesFile(function(err, file) {
-        data.formats.mbtiles.complete = true;
-        var stats = fs.statSync(file);
-        data.formats.mbtiles.size = stats.size;
-        self.cache.cache = data;
-        callback(null, self.cache);
-      });
-    });
-  });
-};
-
-MBTiles.prototype._createMetadataFile = function() {
-  var metadataFile = path.join(this.config.outputDirectory, this.cache.cache.id.toString(), 'xyztiles', 'metadata.json');
-  fs.outputJsonSync(metadataFile, {
-    "name": this.cache.cache.name,
-    "minzoom": this.cache.cache.minZoom+'',
-    "maxzoom": this.cache.cache.maxZoom+'',
-    "bounds": turf.bbox(this.cache.cache.geometry).join(',')
-  });
-}
-
-MBTiles.prototype.createMBTilesFile = function(callback) {
-  var dir = path.join(this.config.outputDirectory, this.cache.cache.id.toString(), 'xyztiles');
-  var mbtilesFile = path.join(this.config.outputDirectory, this.cache.cache.id.toString(), this.cache.cache.id.toString()+'.mbtiles');
-  var args = [dir, mbtilesFile];
-  var command = path.join('..', 'utilities', 'mbutil', 'mb-util')
-  var mbUtil = cp.spawn(command, args, {cwd: __dirname}).on('close', function(code) {
-    callback(null, mbtilesFile);
-  });
-  // not sure why I have to listen to these to process the entire file....
-  mbUtil.stdout.on('data', function(data) {
-    console.log('mbutil stdout: ' + data);
-  });
-  mbUtil.stderr.on('data', function(data) {
-    console.log('mbutil stderr: ' + data);
-  });
-}
 
 MBTiles.prototype.getData = function(minZoom, maxZoom, callback) {
-  var mbtilesFile = path.join(this.config.outputDirectory, this.cache.cache.id.toString(), this.cache.cache.id.toString()+'.mbtiles');
+  console.log('get the data');
+  var mbtilesFilePath = path.join(this.config.outputDirectory, this.cache.cache.id.toString(), 'mbtiles', this.cache.cache.id.toString()+'.mbtiles');
+
+  var zipFilePath = path.join(this.config.outputDirectory, this.cache.cache.id.toString(), 'mbtiles', this.cache.cache.id.toString()+'.zip');
+
+  var filePath = mbtilesFilePath;
+  var extension = '.mbtiles';
+
+  if (fs.existsSync(mbtilesFilePath)) {
+    console.log('mbtiles file exists');
+    filePath = mbtilesFilePath;
+    extension = '.mbtiles';
+  } else if (fs.existsSync(zipFilePath)) {
+    console.log('zip file exists');
+    filePath = zipFilePath;
+    extension = '.zip';
+  } else {
+    console.log('Cache does not exist, go make it');
+    return this.generateCache(function(err, cache) {
+      this.getData(minZoom, maxZoom, callback);
+    }.bind(this));
+  }
+
+  console.log('sending back the file ' + filePath);
   callback(null, {
-    stream: fs.createReadStream(path.join(this.config.outputDirectory, this.cache.cache.id.toString(), this.cache.cache.id.toString()+'.mbtiles')),
-    extension: '.mbtiles'
+    stream: fs.createReadStream(filePath),
+    extension: extension
   });
 };
 
