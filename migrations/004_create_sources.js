21ab39b709f40699d99932ead90e37355f118728
Dan Barela <daniel.barela@gmail.com>
2017-10-09 14:52:38 -0600

    removed all sources except osm

--- migrations/004_create_sources.js
+++ migrations/004_create_sources.js
@@ -6,23 +6,7 @@ exports.up = function(done) {
 
   Source.createSource([{
     name: 'OpenStreetMap',
-    url: 'http://osm.geointapps.org/osm',
-    format: 'xyz'
-  }, {
-    name: 'Mapbox Outdoors',
-    url: 'http://mapbox.geointapps.org:2999/v4/mapbox.mapbox-outdoors',
-    format: 'xyz'
-  }, {
-    name: 'Mapbox Satelite Afternoon',
-    url: 'http://mapbox.geointapps.org:2999/v4/mapbox.satellite-afternoon',
-    format: 'xyz'
-  }, {
-    name: 'Mapbox Light',
-    url: 'http://mapbox.geointapps.org:2999/v4/mapbox.light',
-    format: 'xyz'
-  },{
-    name: 'Mapbox OSM Bright',
-    url: 'http://mapbox.geointapps.org:2999/v4/mapbox.osm-bright',
+    url: 'http://osm.geointservices.io/tiles',
     format: 'xyz'
   }], done);
 };
